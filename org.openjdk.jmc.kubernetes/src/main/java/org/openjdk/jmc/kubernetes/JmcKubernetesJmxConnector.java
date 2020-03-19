4bbf80cb5a5c05782d8c7bced833ef84d36ce22f
Martin Skarsaune <martin.skarsaune@vipps.no>
2020-03-19 12:31:55 +0100

    First cut, with explicit dependencies for kubernetes

---
+++ org.openjdk.jmc.kubernetes/src/main/java/org/openjdk/jmc/kubernetes/JmcKubernetesJmxConnector.java
@@ -0,0 +1,23 @@
+package org.openjdk.jmc.kubernetes;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.management.remote.JMXServiceURL;
+
+import org.jolokia.client.J4pClient;
+import org.jolokia.client.jmxadapter.RemoteJmxAdapter;
+import org.jolokia.kubernetes.client.KubernetesJmxConnector;
+import org.openjdk.jmc.jolokia.JmcJolokiaJmxConnection;
+
+public class JmcKubernetesJmxConnector extends KubernetesJmxConnector {
+
+	public JmcKubernetesJmxConnector(JMXServiceURL serviceURL, Map<String, ?> environment) {
+		super(serviceURL, environment);
+	}
+	
+	@Override
+	protected RemoteJmxAdapter createAdapter(J4pClient client) throws IOException {
+		return new JmcJolokiaJmxConnection(client);
+	}
+}
